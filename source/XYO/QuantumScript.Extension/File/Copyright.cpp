d34322e141dd537497217203d9a624bcc9474cf4
Grigore Stefan <g_stefan@yahoo.com>
2023-03-23 22:51:06 +0200

    Update

--- source/XYO/QuantumScript.Extension/File/Copyright.cpp
+++ source/XYO/QuantumScript.Extension/File/Copyright.cpp
@@ -14,19 +14,19 @@ namespace XYO::QuantumScript::Extension::File::Copyright {
 	static const char *company_ = XYO_QUANTUMSCRIPT_EXTENSION_FILE_COMPANY;
 	static const char *contact_ = XYO_QUANTUMSCRIPT_EXTENSION_FILE_CONTACT;
 
-	std::string copyright() {
+	const char *copyright() {
 		return copyright_;
 	};
 
-	std::string publisher() {
+	const char *publisher() {
 		return publisher_;
 	};
 
-	std::string company() {
+	const char *company() {
 		return company_;
 	};
 
-	std::string contact() {
+	const char *contact() {
 		return contact_;
 	};
 
