649ea6c25f8ce5135cf9d506d24b853b85cdf428
Grigore Stefan <g_stefan@yahoo.com>
2024-08-28 17:05:02 +0300

    Update

--- source/XYO/QuantumScript.Extension/File/Library.cpp
+++ source/XYO/QuantumScript.Extension/File/Library.cpp
@@ -415,8 +415,10 @@ namespace XYO::QuantumScript::Extension::File {
 
 };
 
-#ifdef XYO_PLATFORM_COMPILE_DYNAMIC_LIBRARY
+#ifndef XYO_QUANTUMSCRIPT_EXTENSION_FILE_LIBRARY
+#	ifdef XYO_PLATFORM_COMPILE_DYNAMIC_LIBRARY
 extern "C" XYO_QUANTUMSCRIPT_EXTENSION_FILE_EXPORT void quantumScriptExtension(XYO::QuantumScript::Executive *executive, void *extensionId) {
 	XYO::QuantumScript::Extension::File::initExecutive(executive, extensionId);
 };
+#	endif
 #endif
